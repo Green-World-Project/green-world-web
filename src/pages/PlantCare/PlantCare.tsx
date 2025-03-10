14283a0d979f61cd6c196b313d1c8f2f81d4e9a7
Eric <ericgeorge4477@gmail.com>
2025-03-10 13:31:51 +0200

    feat: Create Plant Care page and header

--- src/pages/PlantCare/PlantCare.tsx
+++ src/pages/PlantCare/PlantCare.tsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
 import AddPlantForm from "../../shared/AddPlantForm";
+import PlantCareList from "./components/PlantCareList";
 
 export default function PlantCare() {
   const [isFormOpen, setIsFormOpen] = useState(false);
   return (
     <div className="container-custom px-[2rem] lg:px-[4rem]">
       <div className="flex flex-row justify-between">
-        <h1 className="text-[#1b5e20] text-4xl font-bold">
-          Plant Care Schedule
-        </h1>
+        <h1 className="text-[#1b5e20] text-4xl font-bold">Plant Care</h1>
         <button
           className="text-center text-[1rem] cursor-pointer font-medium text-white border-[2px] border-[#43a047] bg-[#43a047] px-4 py-2
           rounded-md transition-all hover:bg-[#2e7d32] hover:scale-105 hover:border-[#2e7d32]"
@@ -18,6 +17,7 @@ export default function PlantCare() {
         </button>
         <AddPlantForm isFormOpen={isFormOpen} setIsFormOpen={setIsFormOpen} />
       </div>
+      <PlantCareList />
     </div>
   );
 }
---
+++ src/pages/PlantCare/components/PlantCareList.tsx
@@ -0,0 +1,15 @@
+import PlantCard from "./PlantCard";
+
+const PlantCareList = () => {
+  return (
+    <div>
+      <PlantCard
+        name="Monstera Deliciosa"
+        date="2025-03-02"
+        initialWatered={false}
+      />
+    </div>
+  );
+};
+
+export default PlantCareList;
