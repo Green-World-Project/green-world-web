a52dab43e847ed609d111c8ba4fb9260759ca8d6
Eric <ericgeorge4477@gmail.com>
2025-03-11 17:15:09 +0200

    feat: enhance UploadPlant UI

--- src/pages/Identifiy/Identify.tsx
+++ src/pages/Identifiy/Identify.tsx
@@ -2,7 +2,7 @@ import FloatingDotsBackground from "../../shared/FloatingDotsBackground";
 import UploadPlant from "./components/UploadPlant";
 export default function Identify() {
   return (
-    <div className="container-custom px-[2rem] lg:px-[4rem] py-4 flex items-center justify-center relative min-h-screen bg-gradient-to-br from-lightGreen to-paleGreen">
+    <div className="px-[2rem] lg:px-[4rem] pt-[120px] max-md:pt-10 max-sm:p-0 flex items-center justify-center relative min-h-screen bg-gradient-to-br from-lightGreen to-paleGreen">
       <FloatingDotsBackground numberOfDots={40} />
       <UploadPlant />
     </div>
