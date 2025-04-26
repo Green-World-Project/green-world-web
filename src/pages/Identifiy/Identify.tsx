9408b91f1f8942fd2099a97d850495d5649c9179
Eric <ericgeorge4477@gmail.com>
2025-04-27 02:34:20 +0300

    feat: add useEffect to scroll to top on page load

--- src/pages/Identifiy/Identify.tsx
+++ src/pages/Identifiy/Identify.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import FloatingDotsBackground from "../../shared/FloatingDotsBackground";
 import UploadPlant from "./components/UploadPlant";
 export default function Identify() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="px-[2rem] lg:px-[4rem] pt-[120px] max-md:pt-10 max-sm:p-0 flex items-center justify-center relative min-h-screen bg-gradient-to-br from-green-200 to-green-50 overflow-hidden">
+    <div className="px-[2rem] lg:px-[4rem] pt-[120px] pb-8 max-md:pt-12 max-sm:px-0 flex items-center justify-center relative min-h-screen bg-gradient-to-br from-green-200 to-green-50 overflow-hidden">
       <FloatingDotsBackground numberOfDots={40} />
       <UploadPlant />
     </div>
