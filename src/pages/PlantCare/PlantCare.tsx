b16a92508ab651897ecfc9f7681550d49930ec57
Eric <ericgeorge4477@gmail.com>
2025-04-30 03:00:06 +0300

    feat:modify plant care page and add plantCareList

--- src/pages/PlantCare/PlantCare.tsx
+++ src/pages/PlantCare/PlantCare.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddPlantForm from "../../shared/AddPlantForm";
 import PlantCareList from "./components/PlantCareList";
 
 export default function PlantCare() {
   const [isFormOpen, setIsFormOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div className="container-custom px-[2rem] lg:px-[4rem]">
-      <div className="flex flex-row justify-between">
-        <h1 className="text-[#1b5e20] text-4xl font-bold">Plant Care</h1>
+    <div className="container-custom px-[2rem] lg:px-[4rem] flex flex-col min-h-screen">
+      <div className="flex flex-row justify-between mb-12">
+        <h1 className="text-[#1b5e20] text-4xl font-bold text-left">
+          Plant Care
+        </h1>
         <button
           className="text-center text-[1rem] cursor-pointer font-medium text-white border-[2px] border-[#43a047] bg-[#43a047] px-4 py-2
           rounded-md transition-all hover:bg-[#2e7d32] hover:scale-105 hover:border-[#2e7d32]"
