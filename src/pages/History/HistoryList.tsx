18a9927b4c2fb872614205eb31c532b76e7bbf08
Eric <ericgeorge4477@gmail.com>
2025-05-05 17:12:55 +0300

    fix(HistoryList): improve empty state handling and clean up JSX structure

--- src/pages/History/HistoryList.tsx
+++ src/pages/History/HistoryList.tsx
@@ -73,46 +73,52 @@ function HistoryList() {
 
   return loading ? (
     <LoadingSpinner />
+  ) : !Array.isArray(plants) || plants.length === 0 ? (
+    <div>
+      <p className="text-lg text-gray-500">History is Empty</p>
+    </div>
   ) : (
-    <motion.div
-      className="grid justify-items-center gap-x-6 gap-y-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-16"
-      initial="hidden"
-      animate="visible"
-      variants={{
-        hidden: {},
-        visible: {
-          transition: {
-            staggerChildren: 0.2,
+    <>
+      <motion.div
+        className="grid justify-items-center gap-x-6 gap-y-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-16"
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {},
+          visible: {
+            transition: {
+              staggerChildren: 0.2,
+            },
           },
-        },
-      }}
-    >
-      {plants.map((plant: Plant) => (
-        <motion.div
-          key={plant._id}
-          variants={{
-            hidden: { opacity: 0, y: 20 },
-            visible: { opacity: 1, y: 0 },
-          }}
-          transition={{ duration: 0.4 }}
-          className="w-full"
-        >
-          <IdentifiedPlantCard
-            plant={plant}
-            setSelectedPlantId={setSelectedPlantId}
-            setIsModalOpen={setIsModalOpen}
-            iconSize={24}
-            height=" h-96"
-          />
-        </motion.div>
-      ))}
+        }}
+      >
+        {plants.map((plant: Plant) => (
+          <motion.div
+            key={plant._id}
+            variants={{
+              hidden: { opacity: 0, y: 20 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            transition={{ duration: 0.4 }}
+            className="w-full"
+          >
+            <IdentifiedPlantCard
+              plant={plant}
+              setSelectedPlantId={setSelectedPlantId}
+              setIsModalOpen={setIsModalOpen}
+              iconSize={24}
+              height=" h-96"
+            />
+          </motion.div>
+        ))}
+      </motion.div>
       <ConfirmationModal
         onConfirm={handleRemovePlant}
         onClose={() => setIsModalOpen(false)}
         isOpen={isModalOpen}
         message="Are you sure you want to delete this plant?"
       />
-    </motion.div>
+    </>
   );
 }
 
