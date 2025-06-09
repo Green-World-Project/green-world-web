f63eac3b396cf9db5d5a02c0b4cb340b19c615af
Eric <ericgeorge4477@gmail.com>
2025-06-09 04:35:17 +0300

    refactor(History): remove user greeting logic and simplify header

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,11 +1,8 @@
-import { useContext, useEffect } from "react";
-import { StoreContext } from "../../context/StoreContext";
+import { useEffect } from "react";
 
 import HistoryList from "./HistoryList";
 
 export default function History() {
-  const { userData } = useContext(StoreContext);
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -13,14 +10,7 @@ export default function History() {
   return (
     <div className="container-custom px-[2rem] lg:px-[4rem] flex flex-col">
       <h1 className="text-[#1b5e20] text-4xl font-bold text-left mb-12">
-        {userData ? (
-          <>
-            Hello <span className="text-[#43a047]">{userData.firstName}</span>,
-            here is your plant history
-          </>
-        ) : (
-          "History"
-        )}
+        History
       </h1>
 
       <HistoryList />
